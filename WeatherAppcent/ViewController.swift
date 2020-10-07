a171556d02f1a90ab354fa49a48c111020c0381a
Burak Demir <byburakdemir@gmail.com>
2020-10-07 15:49:30 +0300

    Initial Commit

---
+++ WeatherAppcent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppcent
+//
+//  Created by burak on 7.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
