97afd1e913688c5c76cd42a186921ec83c289ba0
Burak Demir <byburakdemir@gmail.com>
2020-10-14 02:48:17 +0300

    Date format changed at WeatherStatusCV

--- WeatherAppcent/View/WeatherStatusCollectionViewCell.swift
+++ WeatherAppcent/View/WeatherStatusCollectionViewCell.swift
@@ -34,8 +34,7 @@ class WeatherStatusCollectionViewCell: UICollectionViewCell {
         self.imgWeather.kf.indicatorType = .activity
         self.imgWeather.kf.setImage(with: URL(string: imgUrl), placeholder: nil, options: [.transition(.fade(0.2))], progressBlock: nil)
         
-       
-        self.lblDate.text = "\(detail.applicable_date)"
+        
         self.maxTemp.text = "\(String(format: "%.1f", detail.max_temp))℃"
         self.minTemp.text = "\(String(format: "%.1f", detail.min_temp))℃"
         self.windSpeed.text = "\(detail.wind_direction_compass) \(String(format: "%.f", detail.wind_speed))mph"
@@ -45,7 +44,45 @@ class WeatherStatusCollectionViewCell: UICollectionViewCell {
         self.lblPredictability.text = "\(detail.predictability)%"
         
         
+        let formatted = DateFormatter()
+        formatted.locale = Locale(identifier: "en_US_POSIX")
+        formatted.dateFormat = "yyyy-MM-dd"
         
+        func getFormattedTime(inputTime inputStr: String) -> String {
+            
+            var formattedTime:String?
+            
+            if let time = formatted.date(from: inputStr) {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "E d MMM"
+                formattedTime =  formatter.string(from: time)
+            }
+            return formattedTime ?? "unknown"
+        }
+        
+        
+        let current = Date()
+        let today = formatted.string(from: current)
 
+        let dates = Date()
+        let tomorrowDate = Calendar.current.date(byAdding: .day, value: 1, to: dates)!
+        let tomorrow = formatted.string(from: tomorrowDate)
+        
+        
+        print(tomorrow)
+
+        if detail.applicable_date == today {
+            self.lblDate.text = "Today"
+        }else if detail.applicable_date == tomorrow {
+            self.lblDate.text = "Tomorrow"
+        }
+        else{
+            let dateTime = getFormattedTime(inputTime: detail.applicable_date)
+            self.lblDate.text = dateTime
+        }
+        
     }
 }
+
+
+
